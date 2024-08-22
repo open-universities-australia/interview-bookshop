2ad072be6e7c3e599911349827411eb9c7387314
Dejan Vasic <dejanvasic24@gmail.com>
2024-08-22 11:44:20 +1000

    Reducing timeout

--- src/app/api/books/route.ts
+++ src/app/api/books/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1500));
   return NextResponse.json(books);
 }
